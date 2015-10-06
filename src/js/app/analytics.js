cb85b3d7078b8f860cb476076b5cabe622adaa7d
Peter Warman <petewarman@me.com>
2015-10-06 16:35:40 +0100

    add protocol for analytics

--- src/js/app/analytics.js
+++ src/js/app/analytics.js
@@ -11,7 +11,7 @@ define( [],
       a.async = 1;
       a.src = g;
       m.parentNode.insertBefore( a, m )
-    })( window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga' );
+    })( window, document, 'script', 'http://www.google-analytics.com/analytics.js', 'ga' );
 
     ga( 'create', 'UA-67811141-1', 'auto' );
     ga( 'send', 'pageview' );
